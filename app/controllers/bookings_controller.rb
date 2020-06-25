8aac2959d3c0f0ac5449ee231b060a0e435db516
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-06-25 23:08:10 +0100

    create bookings and venues controller actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,32 @@
 class BookingsController < ApplicationController
   def index
+    @user = current_user
+    @booking = Bookings.all
   end
 
   def show
+    @user = current_user
+    @booking = Booking.find(params[:id])
   end
 
   def new
   end
 
-  def show
+  def create
+    @venue = Venue.find(params[:venue_id])
+    @bookings = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.venue = @venue
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render "venues/show"
+    end
   end
 
-  def create
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_on, :end_on)
   end
 end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,17 +1,54 @@
 class VenuesController < ApplicationController
+  before_action :set_venue, only: [:show, :edit, :update, :destroy]
   def index
-    venues = Venue.all
+    @venues = Venue.all
   end
 
   def show
+    @booking = Booking.new
   end
 
   def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    @venue.owner = current_user
+    if @venue.save
+      redirect_to my_own_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @venue.update(venue_params)
+      redirect_to venue_path(@venue), notice: 'Venue was successfully updated.'
+    else
+      render :edit
+
   end
 
   def destroy
+    @venue.destroy
+    redirect_to venues_path(:search => { query: "" })
   end
 
   def my_own
+    @my_venues = Venue.where(user_id: current_user.id)
+  end
+
+  private
+
+  def set_venue
+    @venue = Venue.find(params[:id])
+  end
+
+  def venue_params
+    params.require(:venue).permit(:description, :price, :owner_id, :name, :image, :category, :address)
   end
 end
