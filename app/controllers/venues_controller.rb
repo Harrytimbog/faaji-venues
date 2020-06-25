04df4c2ec84985f330cbc01b80a316b6a8e132bf
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-06-25 14:44:32 +0100

    put user session pages in bootstrap grids

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,17 @@
+class VenuesController < ApplicationController
+  def index
+    venues = Venue.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def my_own
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :moniker, presence: true, allow_blank: false
   validates :email, presence: true, allow_blank: false, uniqueness: true
   validates :encrypted_password, presence: true, allow_blank: false
 
