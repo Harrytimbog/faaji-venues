2be6d0e83e45474a6538a1b8d7f743d267f9abd1
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-06-25 14:15:40 +0100

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "venues/my_own", to: "venues#my_own", as: :my_own
+  resources :venues do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show]
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
